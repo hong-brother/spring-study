776fe86f841aab59dd85337bd698b5d7145dfcef
hong-brother <robbins.hs@gmail.com>
2023-11-22 23:38:47 +0900

    init

---
+++ redis-cache/src/main/java/io/hsnam/rediscache/RedisCacheApplication.java
@@ -0,0 +1,13 @@
+package io.hsnam.rediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCacheApplication.class, args);
+    }
+
+}
---
+++ redis-cache/src/test/java/io/hsnam/rediscache/RedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hsnam.rediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
